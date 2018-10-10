1618b7af37fafae01b3936539b652c2d9d995338
azocher <anna.zocher@gmail.com>
2018-10-09 19:50:55 -0700

    pushing first commit of project to github from local terminal

---
+++ script.js
@@ -0,0 +1,11 @@
+// saving found secret div to var found
+var found = document.getElementById("seven");
+var starting_width = window.innerWidth;
+
+
+
+if (found != null) {
+  console.log("hi!");
+} else {
+  console.log(found);
+}
